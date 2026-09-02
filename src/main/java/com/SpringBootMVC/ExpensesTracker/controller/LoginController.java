704c3a999f656414c78ba1b14774bcb58fd2bf1f
swapnalimore371 <swpnalimore371@gmail.com>
2026-09-02 20:10:52 +0530

    Initial commit - Expenses Tracker

---
+++ src/main/java/com/SpringBootMVC/ExpensesTracker/controller/LoginController.java
@@ -0,0 +1,16 @@
+package com.SpringBootMVC.ExpensesTracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showLoginPage")
+    public String showLoginPage(){
+          //System.out.println("Showing login page");
+        return "login-page";
+    }
+
+  
+}
